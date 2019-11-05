e777d0f17bc4b04e2c6530177177e7b85a55e97f
Gunslinger <gunslinger-ovo@qq.com>
2019-11-05 15:34:17 +0800

    Add auto building

---
+++ build/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -eu
+
+NCS_DIR="$(pwd)"
+DEPS_DIR="$NCS_DIR/deps"
+SMBRANCH="master"
+
+if [[ ! -d "$DEPS_DIR" ]]; then
+	mkdir -p "$DEPS_DIR"
+fi
+
+cd "$DEPS_DIR" || exit
+
+# Boost
+echo "Building Boost"
+if [[ ! -d "boost" ]]; then
+	if [[ ! -f "boost_1_69_0.tar.gz" ]]; then
+		wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz && tar -xzf boost_1_69_0.tar.gz
+	fi
+	cd boost_1_69_0
+	./bootstrap.sh --with-libraries=all --with-toolset=gcc
+	./b2 install --prefix="$DEPS_DIR/boost"
+	cd "$NCS_DIR" || exit
+fi
+
+# SourceMod
+echo "Getting SourceMod"
+if [[ ! -d "sourcemod" ]]; then
+	git clone https://github.com/alliedmodders/sourcemod --recursive --branch "$SMBRANCH"
+fi
+
+echo "Building extension"
+cd "$NCS_DIR" || exit
+make SMSDK="$DEPS_DIR/sourcemod" BOOST="$DEPS_DIR/boost"
